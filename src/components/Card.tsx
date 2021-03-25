2797f9bb3de63e65eb7c242f45b9ab2b69f3c821
Bhekani Khumalo <bhekanik@gmail.com>
2021-03-25 22:46:25 +0000

    start

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,11 +3,13 @@ import styles from "../styles/card.module.css";
 
 interface Props {
   theme?: string;
+  desc?: string;
 }
 
-export const Card: FC<Props> = ({ theme, children }) => {
+export const Card: FC<Props> = ({ theme, desc, children }) => {
   return (
     <div className={theme === "dark" ? styles.cardDark : styles.card}>
+      {desc && <span className={styles.desc}>{desc}</span>}
       {children}
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import Greeting from "../components/Greeting";
+import GreetingHooks from "../components/GreetingHooks";
 import styles from "../styles/Home.module.css";
 
 const hooks = () => {
   return (
     <div className={styles.container}>
       <Greeting />
+      {/* <GreetingHooks /> */}
     </div>
   );
 };
